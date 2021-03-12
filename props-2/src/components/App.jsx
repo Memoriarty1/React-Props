0e64a3a64b574d7c9e639dee97258f474a5e1732
Ignatenko Igor <ignatenko.igor.ol@gmail.com>
2021-03-12 15:32:14 +0300

    refactored code and add new components

--- props-2/src/components/App.jsx
+++ props-2/src/components/App.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import Card from './Card';
 import contacts from '../contacts';
+import Avatar from './Avatar';
+
+
+function createCard(contact) {
+  return <Card 
+  key={contact.id}
+    id={contact.id}
+    img={contact.imgURL} 
+    name={contact.name} 
+    tel={contact.phone} 
+    email={contact.email}
+   />
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-        {contacts.map((index, value) => {
+      <Avatar img='https://icdn.lenta.ru/images/2018/08/23/11/20180823110031977/pic_33c12aa3a20161bbd2e3962a8dc6254d.jpg' />
+      {contacts.map(createCard)}
+        {/* {contacts.map((index, value) => {
           return <Card  key ={value} img={index.imgURL} name={index.name} tel={index.phone} email={index.email} />
-        })}
+        })} */}
       {/* <Card
         img={contacts[0].imgURL}
         name={contacts[0].name}
